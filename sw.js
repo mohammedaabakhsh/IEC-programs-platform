d2435a92e53e5b2a8240d521b211eab579baa773
mohammedaabakhsh <mohammed.a.a.bakhsh@gmail.com>
2026-07-19 07:31:52 +0300

    chore: refresh Arabic database interface assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iec-platform-v2.0.2-sidebar-scroll';
+const CACHE='iec-platform-v2.1.0-arabic-database';
 const ASSETS=['./','./index.html','./styles.css','./evaluation-v2.css','./app.js','./evaluation-v2.js','./cloud-sync.js','./question-bank.js','./question-bank.css','./program-features.js','./program-features.css','./advanced-features.js','./advanced-features.css','./completion-features.js','./completion-features.css','./insights-reports.js','./insights-reports.css','./launch-features.js','./launch-features.css','./strategic-suite.js','./strategic-suite.css','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
