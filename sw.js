f2fba4741c3b55926959d69d8a53be3ff3e612e5
mohammedaabakhsh <mohammed.a.a.bakhsh@gmail.com>
2026-07-19 06:30:16 +0300

    fix: cache complete offline application assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='iec-platform-v1.0.0-rc1';
-const ASSETS=['./','./index.html','./styles.css','./evaluation-v2.css','./app.js','./evaluation-v2.js','./question-bank.js','./question-bank.css','./program-features.js','./program-features.css','./advanced-features.js','./advanced-features.css','./completion-features.js','./completion-features.css','./insights-reports.js','./insights-reports.css','./launch-features.js','./launch-features.css','./manifest.webmanifest'];
+const CACHE='iec-platform-v1.0.0-rc1-2';
+const ASSETS=['./','./index.html','./styles.css','./evaluation-v2.css','./app.js','./evaluation-v2.js','./question-bank.js','./question-bank.css','./program-features.js','./program-features.css','./advanced-features.js','./advanced-features.css','./completion-features.js','./completion-features.css','./insights-reports.js','./insights-reports.css','./launch-features.js','./launch-features.css','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
